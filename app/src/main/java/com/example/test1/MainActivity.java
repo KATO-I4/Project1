b0c75e30642e04a451329dea368c1a29afc0e477
KATO-I4 <ji8537645@gmail.com>
2022-05-17 10:28:10 +0900

    test

---
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=findViewById(R.id.buttonChangeMessage);
+        button.setOnClickListener(new MyOnClickListener());
+
+
+    }
+
+    class MyOnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            TextView text = findViewById(R.id.textViewMessage);
+            text.setText("こんにちはandroid！");
+        }
+    }
+}
