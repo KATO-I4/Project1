0edf28c3f7d294499626c7c4cc3cb79ccdead6b9
KATO-I4 <ji8537645@gmail.com>
2022-05-18 15:30:22 +0900

    15:29

--- app/src/main/java/com/example/test1/reviewup.java
+++ app/src/main/java/com/example/test1/reviewup.java
@@ -1,6 +1,27 @@
 package com.example.test1;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class reviewup extends AppCompatActivity {
+public class reviewup  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //activity_subレイアウトをセット
+        setContentView(R.layout.reviewup);
+
+        Button returnButton = findViewById(R.id.return_btn);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
 }
