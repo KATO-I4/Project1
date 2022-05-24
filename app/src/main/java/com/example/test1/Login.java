725f9e405ae309300118fa2b7d3828667a8f0817
KATO-I4 <ji8537645@gmail.com>
2022-05-24 14:25:45 +0900

    0524_2

--- app/src/main/java/com/example/test1/Login.java
+++ app/src/main/java/com/example/test1/Login.java
@@ -2,121 +2,43 @@ package com.example.test1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class Login extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-        EditText userEmail = (EditText) findViewById(R.id.editTextTextEmailAddress);
-        EditText userPass = (EditText) findViewById(R.id.editTextTextPassword);
-        Button loginButton = (Button) findViewById(R.id.sign_in_btn);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        //loginレイアウトをセット
+        setContentView(R.layout.login);
+
+        Button returnButton = findViewById(R.id.return_btn);
+        returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String userEmail_text = userEmail.getText().toString();
-                String userPass_text = userPass.getText().toString();
-                signIn(userEmail_text,userPass_text);
-                Log.d("TAG", userPass_text+"Value is: " + userEmail_text);
+            public void onClick(View v) {
+                finish();
             }
         });
-        mAuth = FirebaseAuth.getInstance();
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        //レビュー詳細への画面遷移用のボタン
+        Button Button_to_Login = findViewById(R.id.sign_in_btn);
 
-            Intent intent = new Intent(Login.this, MainActivity.class);
-            startActivity(intent);
-        }else{
-            reload();
-        }
-    }
-    private void reload() { }
-    private void updateUI(FirebaseUser user) {
+        //ボタン押下時
+        Button_to_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    }
+                //login_ok遷移
+                Intent intent = new Intent(getApplication(), Login_OK.class);
+                startActivity(intent);
+            }
 
-    private void signIn(String email, String password) {
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("TAG", "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                            Intent intent = new Intent(Login.this, MainActivity.class);
-                            startActivity(intent);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("TAG", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(Login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-        // [END sign_in_with_email]
+        });
     }
-    private void signUp(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("TAG", "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(Login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-    }
-    private void sendEmailVerification() {
-        // Send verification email
-        // [START send_email_verification]
-        final FirebaseUser user = mAuth.getCurrentUser();
-        user.sendEmailVerification()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        // Email sent
-                    }
-                });
-        // [END send_email_verification]
-    }
-
 
 }
--- app/src/main/java/com/example/test1/Login_OK.java
+++ app/src/main/java/com/example/test1/Login_OK.java
@@ -7,35 +7,37 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Read  extends AppCompatActivity {
+
+public class Login_OK extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //reviewinspectionレイアウトをセット
-        setContentView(R.layout.read);
+        //loginレイアウトをセット
+        setContentView(R.layout.login_ok);
 
         Button returnButton = findViewById(R.id.return_btn);
         returnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
-
             }
         });
 
-        Button Change_Button1 = findViewById(R.id.change_btn1);
-        Change_Button1.setOnClickListener(new View.OnClickListener() {
+        //レビュー詳細への画面遷移用のボタン
+        Button Button_to_QR = findViewById(R.id.button3);
+
+        //ボタン押下時
+        Button_to_QR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //readに画面遷移
-               //setContentView(R.layout.map);
-                //Recommendに画面遷移
-                Intent intent = new Intent(getApplication(), MAP.class);
-                startActivity(intent);
 
+                Intent intent = new Intent(getApplication(), Read.class);
+                startActivity(intent);
             }
+
         });
     }
 
